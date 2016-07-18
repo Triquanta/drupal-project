e00787cdfd0ddcba6fc67409a18429bdad39c43b
dmsmidt <dmsmidt@gmail.com>
2016-07-18 14:05:53 +0200

    Improve CLI error when no host is configured (Drupal default is used).

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -7,7 +7,16 @@
  *
  * @see default.settings.php
  */
-exit(sprintf('Error: Host <em>%s</em> is not configured.', $_SERVER['HTTP_HOST']));
+
+if (php_sapi_name() == 'cli') {
+  $error_prefix = "Error from " . __FILE__;
+}
+else {
+  $error_prefix = "Error";
+}
+
+exit(sprintf($error_prefix . ': Host <em>%s</em> is not configured.' . PHP_EOL, $_SERVER['HTTP_HOST']));
+
 
 // If you see this message, check if the host name is configured in
 // sites/sites.php or a directory in sites/ exists with this name.
